c9aa4f91b7ec4564f1fc92b7e0286a6f5d99d840
Aïmen <mokrani.aimen@gmail.com>
2018-02-19 15:47:41 +0100

    Récupération de ligne du csv

--- main.c
+++ main.c
@@ -3,7 +3,17 @@
 
 int main()
 {
-    printf("Hello world!, voici jonathan\n");
-    printf("Hello world!, voici aimen\n");
+    FILE* file = fopen("Csv_test.csv", "r");
+
+    char line[1024];
+
+    while (fgets(line, sizeof(line), file)) {
+        char* tmp = strdup(line);
+        printf(tmp);
+
+        // Appeler la fonction de JO et Vincent
+    }
+
+
     return 0;
 }
